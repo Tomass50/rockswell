6330d8d64052f95d301f1cc4d900d6daeca17162
KramerEd <danat7531@gmail.com>
2023-09-11 16:50:25 +0300

    add animation for logo

--- src/components/Header/components/Logo.tsx
+++ src/components/Header/components/Logo.tsx
@@ -1,11 +1,33 @@
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import stylesHeader from "@/styles/header.module.css";
 import LogoImage from "../../../assets/header/Logo.svg";
 const Logo = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    if (window !== undefined) {
+      window.addEventListener("scroll", () => {
+        setScrolled(window.scrollY > 0);
+      });
+    }
+    return () => {
+      if (window !== undefined) {
+        window.removeEventListener("scroll", () => {
+          setScrolled(window.scrollY > 0);
+        });
+      }
+    };
+  }, []);
+
   return (
-    <motion.div animate={{ height: "100%" }}>
+    <motion.div
+      animate={{
+        height: scrolled ? "100%" : "150%",
+        y: scrolled ? "0%" : 48,
+      }}
+    >
       <Image
         className={stylesHeader["header-logo"]}
         src={LogoImage}
